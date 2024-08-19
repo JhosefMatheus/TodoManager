d84809f259ee904b23d16ab190fc7638c49e5bb1
JhosefMatheus <jhosef.dev@gmail.com>
2024-08-19 01:13:28 -0300

    sync

--- obj/Debug/net8.0/TodoManager.AssemblyInfo.cs
+++ obj/Debug/net8.0/TodoManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89541bb01061a987b664608eddd9fbfea1409885")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3273b4f72f066f3f55af8ff32bbb2b973770d98")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
