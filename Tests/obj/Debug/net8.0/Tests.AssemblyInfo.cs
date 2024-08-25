cb22821e4e2cf81ed8c17d34cd15ded55742a872
JhosefMatheus <jhosef.dev@gmail.com>
2024-08-24 23:50:09 -0300

    sync

--- Tests/obj/Debug/net8.0/Tests.AssemblyInfo.cs
+++ Tests/obj/Debug/net8.0/Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+208e0f72559d3ebbcace512ebefb2021162578fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ed2bd1490ea1276400bec596fbcad67256b41fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
